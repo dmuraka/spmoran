1c36aff05f67f92d4f3fd557ab6dae35e53600a2
dmuraka <45805390+dmuraka@users.noreply.github.com>
2022-08-12 22:21:03 +0900

    Update boston_data_example.R

--- sample_code_nongaussian/boston_data_example.R
+++ sample_code_nongaussian/boston_data_example.R
@@ -4,6 +4,7 @@ ipkgs <- rownames(installed.packages())
 if (!("spdep" %in% ipkgs)) install.packages("spdep")
 if (!("sf" %in% ipkgs)) install.packages("sf")
 if (!("spmoran" %in% ipkgs)) install.packages("spmoran")
+if(packageVersion("spmoran") < "0.2.2.5") install.packages("spmoran")
 
 library(spdep);library(sf);library(spmoran)
 
